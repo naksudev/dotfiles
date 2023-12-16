50a7612eb21f24e64db123794874702d3e54340e
naksudev <xersinn@gmail.com>
2023-12-16 19:08:47 +0100

    :wrench: Added wallpaper switch function

---
+++ files/hyprland/wallpaper.py
@@ -0,0 +1,55 @@
+import os
+import subprocess
+import time
+import argparse
+
+WALLPAPER_PATH_NSFW = "/home/nak/Pictures/Wallpapers/Nier/NSFW"
+WALLPAPER_PATH_SFW = "/home/nak/Pictures/Wallpapers/Nier/SFW"
+
+MODE_FILE = "/home/nak/.config/hypr/wallpaper_mode"  
+
+def read_mode():
+    try:
+        with open(MODE_FILE, 'r') as file:
+            return file.read().strip()
+    except FileNotFoundError:
+        return "SFW"  
+
+def write_mode(mode):
+    with open(MODE_FILE, 'w') as file:
+        file.write(mode)
+
+def toggle_nsfw_mode():
+    current_mode = read_mode()
+    new_mode = "NSFW" if current_mode == "SFW" else "SFW"
+    write_mode(new_mode)
+    change_wallpaper()
+
+def change_wallpaper():
+    current_mode = read_mode()
+    wallpaper_path = WALLPAPER_PATH_NSFW if current_mode == "NSFW" else WALLPAPER_PATH_SFW
+
+    image_files = [os.path.join(wallpaper_path, file) for file in os.listdir(wallpaper_path) if os.path.isfile(os.path.join(wallpaper_path, file))]
+
+    if image_files:
+        chosen_image = image_files[int(time.time()) % len(image_files)]
+        subprocess.run(["/usr/bin/swww", "img", chosen_image])
+    else:
+        print("Aucune image trouvée dans le répertoire.")
+        exit(1)
+
+if __name__ == "__main__":
+    # Arguments Parser
+    parser = argparse.ArgumentParser(description='Script de changement de fond d\'écran')
+    parser.add_argument('-toggle-nsfw', action='store_true', help='Basculer le mode NSFW')
+
+    args = parser.parse_args()
+
+    if args.toggle_nsfw:
+        toggle_nsfw_mode()
+    else:
+        interval = 300
+        while True:
+            change_wallpaper()
+            time.sleep(interval)
+
