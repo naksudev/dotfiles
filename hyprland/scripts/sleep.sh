bd7f5deb6c7c5542492729eef7a54fafc9e9d099
naksudev <xersinn@gmail.com>
2024-01-23 11:34:30 +0100

    :wrench: hyprland_scripts: Updated grace of swaylock

--- hyprland/scripts/sleep.sh
+++ hyprland/scripts/sleep.sh
@@ -1,3 +1,3 @@
-swayidle -w timeout 120 'swaylock -f --config ~/.config/swaylock/config --grace 1' \
+swayidle -w timeout 120 'swaylock -f --config ~/.config/swaylock/config --grace 2' \
 	    timeout 180 'systemctl suspend' \
 	    before-sleep 'swaylock -f --config ~/.config/swaylock/config' &
