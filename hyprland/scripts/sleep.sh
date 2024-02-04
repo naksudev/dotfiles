7cd5f27549550779d303af241c55066b4beffcaf
naksudev <xersinn@gmail.com>
2024-02-04 21:19:15 +0100

    :wrench: hyprland_scripts: Increased timeout for swaylock

--- hyprland/scripts/sleep.sh
+++ hyprland/scripts/sleep.sh
@@ -1,3 +1,3 @@
-swayidle -w timeout 120 'swaylock -f --config ~/.config/swaylock/config --grace 2' \
-	    timeout 180 'systemctl suspend' \
+swayidle -w timeout 300 'swaylock -f --config ~/.config/swaylock/config --grace 2' \
+	    timeout 600 'systemctl suspend' \
 	    before-sleep 'swaylock -f --config ~/.config/swaylock/config' &
