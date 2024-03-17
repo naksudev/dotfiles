ced4ea2b030962b889e4d49ee77d43053720bb24
naksudev <xersinn@gmail.com>
2024-03-17 21:09:37 +0100

    :boom: :wrench: :sparkling: hypridle/hyprland: Using hypridle instead of my sleep script

--- hyprland/scripts/sleep.sh
+++ hyprland/scripts/sleep.sh
@@ -1,3 +1,5 @@
+# DEPRECATED
+# I use hypridle instead.
 swayidle -w timeout 300 'swaylock -f --config ~/.config/swaylock/config --grace 2' \
 	    timeout 600 'systemctl suspend' \
 	    before-sleep 'swaylock -f --config ~/.config/swaylock/config' &
