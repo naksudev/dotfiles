1ad9bf840d3b48dd51cefc3f42b16d202374773f
Naksu <xersinn@gmail.com>
2025-10-21 01:43:21 +0200

    :package: Replaced markdown plugin

---
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,7 @@
+local Plugin = { "MeanderingProgrammer/render-markdown.nvim" }
+
+Plugin.dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }
+
+Plugin.opts = {}
+
+return Plugin
