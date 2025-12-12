3d4601144c32abc54f7b7d6cee971afba73b6efc
Naksu <xersinn@gmail.com>
2025-12-12 21:51:17 +0100

    :wrench: Stupid bind removed

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -1,5 +1,3 @@
 -- Barbar
 vim.keymap.set('n', '<A-Left>', '<Cmd>BufferPrevious<CR>', { silent = true, noremap = true })
 vim.keymap.set('n', '<A-Right>', '<Cmd>BufferNext<CR>', { silent = true, noremap = true })
-vim.keymap.set('n', '<C-n>', '<Cmd>enew<CR>', { silent = true, noremap = true })
-vim.keymap.set('n', '<C-w>', '<Cmd>BufferClose<CR>', { silent = true, noremap = true })
