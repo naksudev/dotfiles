937f3bebf336ca0c4c94043737022dc40f092697
naksudev <xersinn@gmail.com>
2026-07-21 18:29:03 +0200

    feat(nvim): yanked text highlighted briefly

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,3 +1,9 @@
 -- [/ Autocmds /]
 
--- None for now...
+-- Highlight yanked text briefly
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank({ higroup = 'HiSearch', timeout = 800 })
+  end,
+})
