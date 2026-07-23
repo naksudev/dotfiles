0c142be488d46437e0a13e16f08b1125a99f496c
naksudev <xersinn@gmail.com>
2026-07-23 13:39:07 +0200

    fix(nvim): yank highlight autocmd not working

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -4,6 +4,6 @@
 vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
   callback = function()
-    vim.highlight.on_yank({ higroup = 'HiSearch', timeout = 800 })
+    vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 350 })
   end,
 })
