2037d25e89f5d6ec42f64bb6d22f249d32838c4d
naksudev <xersinn@gmail.com>
2026-08-04 18:23:58 +0200

    fix(hypr): AQ_NO_KMS_REQUIREMENT not required

--- .config/hypr/src/env.lua
+++ .config/hypr/src/env.lua
@@ -2,4 +2,4 @@
 -- https://wiki.hyprland.org/Configuring/Environment-variables/
 
 hl.env('XCURSOR_SIZE', '12')
-hl.env('AQ_NO_KMS_REQUIREMENT', '1') -- See: https://wiki.hypr.land/Configuring/Advanced-and-Cool/Virtual-GPU/
+--hl.env('AQ_NO_KMS_REQUIREMENT', '1') -- See: https://wiki.hypr.land/Configuring/Advanced-and-Cool/Virtual-GPU/
